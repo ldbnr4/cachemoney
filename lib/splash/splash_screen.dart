082266e03ac13e023bc2befa2f4b2e482faf7b5d
FlutLab.io <>
2021-03-20 21:31:37 +0000

    Autosave

--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -27,7 +27,7 @@ class _SplashScreenState extends State<SplashScreen> {
                     // Child text spans will inherit styles from parent
                     style: new TextStyle(
                       fontSize: 14.0,
-                      color: Colors.black,
+                      color: Colors.white,
                     ),
                     children: <TextSpan>[
                       new TextSpan(text: 'Hello'),
