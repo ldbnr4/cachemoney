bc4a9628c25418080dc0fb3fad1a5f1670d28481
FlutLab.io <>
2021-03-20 21:51:07 +0000

    Autosave

--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -22,51 +22,51 @@ class _SplashScreenState extends State<SplashScreen> {
         backgroundColor: Colors.black,
         body: SingleChildScrollView(
           physics: AlwaysScrollableScrollPhysics(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/icons/cachemoney-light.png'),
-              Center(
-                child: new RichText(
-                  text: new TextSpan(
-                    // Note: Styles for TextSpans must be explicitly defined.
-                    // Child text spans will inherit styles from parent
-                    style: new TextStyle(
-                      fontSize: 35.0,
-                      color: Colors.white,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignIn()));
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/icons/cachemoney-light.png'),
+                Center(
+                  child: new RichText(
+                    text: new TextSpan(
+                      // Note: Styles for TextSpans must be explicitly defined.
+                      // Child text spans will inherit styles from parent
+                      style: new TextStyle(
+                        fontSize: 35.0,
+                        color: Colors.white,
+                      ),
+                      children: <TextSpan>[
+                        new TextSpan(text: 'Keep your '),
+                        new TextSpan(text: 'head ', style: new TextStyle(fontWeight: FontWeight.bold, color: Colors.amber)),
+                        new TextSpan(text: 'up,'),
+                        new TextSpan(text: '\n'),
+                        new TextSpan(text: 'Keep your '),
+                        new TextSpan(text: 'bread ', style: new TextStyle(fontWeight: FontWeight.bold, color: Colors.green)),
+                        new TextSpan(text: 'up'),
+                      ],
                     ),
-                    children: <TextSpan>[
-                      new TextSpan(text: 'Keep your '),
-                      new TextSpan(text: 'head ', style: new TextStyle(fontWeight: FontWeight.bold, color: Colors.amber)),
-                      new TextSpan(text: 'up,'),
-                      new TextSpan(text: '\n'),
-                      new TextSpan(text: 'Keep your '),
-                      new TextSpan(text: 'bread ', style: new TextStyle(fontWeight: FontWeight.bold, color: Colors.green)),
-                      new TextSpan(text: 'up'),
-                    ],
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Center(
-                child: Text(
-                  'Best way to invest & save money for the future just by using your phone',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 15, color: Colors.grey.shade600),
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              //   GestureDetector(
-              //     onTap: () {
-              //       Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignIn()));
-              //     },
-              //   )
-            ],
+                Center(
+                  child: Text(
+                    'Best way to invest & save money for the future just by using your phone',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 15, color: Colors.grey.shade600),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
           ),
         ),
       ),
