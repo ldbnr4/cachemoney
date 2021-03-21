79194f9a3ae4e4896b46c165d945637a4be4824a
FlutLab.io <>
2021-03-21 00:52:42 +0000

    Autosave

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -184,116 +184,118 @@ class _HomeScreenState extends State<HomeScreen> {
                       scrollDirection: Axis.horizontal,
                       padding: EdgeInsets.all(10),
                       children: [
-                        Container(
-                          height: 160,
-                          width: MediaQuery.of(context).size.width * .5,
-                          decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                begin: Alignment.topLeft,
-                                end: Alignment.bottomRight,
-                                colors: [
-                                  Colors.grey.shade800,
-                                  Colors.black,
-                                ],
-                              ),
-                              borderRadius: BorderRadius.circular(30)),
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  height: 10,
+                        Card(
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          child: Container(
+                            height: 160,
+                            width: MediaQuery.of(context).size.width * .5,
+                            decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                  colors: [
+                                    Colors.grey.shade800,
+                                    Colors.black,
+                                  ],
                                 ),
-                                ListTile(
-                                  dense: true,
-                                  leading: Container(
-                                    height: 40,
-                                    width: 40,
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      gradient: LinearGradient(
-                                        begin: const FractionalOffset(0.0, 1.0),
-                                        end: const FractionalOffset(1.0, 0.0),
-                                        // stops: [0.2, 0.5, 0.7, 0.9],
-                                        colors: [
-                                          Color(0xff8AF2FD),
-                                          Color(0xff5BC7B1),
-                                          // Color(0xff7DCDE1),
-                                          Color(0xff148399),
-                                        ],
-                                      ),
-                                    ),
-                                    child: Center(
-                                      child: Icon(
-                                        FontAwesomeIcons.apple,
-                                        color: Colors.white,
-                                      ),
-                                    ),
+                                borderRadius: BorderRadius.circular(30)),
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    height: 10,
                                   ),
-                                  title: Text(
-                                    "AAPL",
-                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
-                                  ),
-                                  subtitle: Text(
-                                    "Apple Inc",
-                                    style: TextStyle(fontSize: 15, color: Colors.grey.shade700),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 15, top: 15),
-                                  child: Text(
-                                    "Portfolio",
-                                    style: TextStyle(color: Colors.grey.shade500, fontSize: 16),
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 15),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "\$12,484",
-                                        style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                                      ),
-                                      SizedBox(
-                                        width: 10,
-                                      ),
-                                      Container(
-                                        // color: Color(0xff5BC7B1),
-                                        // width: 80,
-                                        // height: 30,
-                                        child: Row(
-                                          children: [
-                                            Row(
-                                              mainAxisAlignment: MainAxisAlignment.center,
-                                              crossAxisAlignment: CrossAxisAlignment.center,
-                                              children: [
-                                                Icon(
-                                                  Icons.arrow_drop_up,
-                                                  color: Colors.white,
-                                                ),
-                                                Text(
-                                                  "+ 2.5%",
-                                                  style: TextStyle(color: Colors.white, fontSize: 15),
-                                                ),
-                                              ],
-                                            )
+                                  ListTile(
+                                    dense: true,
+                                    leading: Container(
+                                      height: 40,
+                                      width: 40,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        gradient: LinearGradient(
+                                          begin: const FractionalOffset(0.0, 1.0),
+                                          end: const FractionalOffset(1.0, 0.0),
+                                          // stops: [0.2, 0.5, 0.7, 0.9],
+                                          colors: [
+                                            Color(0xff8AF2FD),
+                                            Color(0xff5BC7B1),
+                                            // Color(0xff7DCDE1),
+                                            Color(0xff148399),
                                           ],
                                         ),
                                       ),
-                                    ],
+                                      child: Center(
+                                        child: Icon(
+                                          FontAwesomeIcons.apple,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                    title: Text(
+                                      "AAPL",
+                                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+                                    ),
+                                    subtitle: Text(
+                                      "Apple Inc",
+                                      style: TextStyle(fontSize: 15, color: Colors.grey.shade700),
+                                    ),
                                   ),
-                                ),
-                              ],
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 15, top: 15),
+                                    child: Text(
+                                      "Portfolio",
+                                      style: TextStyle(color: Colors.grey.shade500, fontSize: 16),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 15),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "\$12,484",
+                                          style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Container(
+                                          // color: Color(0xff5BC7B1),
+                                          // width: 80,
+                                          // height: 30,
+                                          child: Row(
+                                            children: [
+                                              Row(
+                                                mainAxisAlignment: MainAxisAlignment.center,
+                                                crossAxisAlignment: CrossAxisAlignment.center,
+                                                children: [
+                                                  Icon(
+                                                    Icons.arrow_drop_up,
+                                                    color: Colors.white,
+                                                  ),
+                                                  Text(
+                                                    "+ 2.5%",
+                                                    style: TextStyle(color: Colors.white, fontSize: 15),
+                                                  ),
+                                                ],
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                        Divider(width: 10),
                         Container(
                           height: 160,
                           width: MediaQuery.of(context).size.width * .5,
