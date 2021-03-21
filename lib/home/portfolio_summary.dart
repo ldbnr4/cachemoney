b5262570c9fb01cf360fa706745b39095188f39d
FlutLab.io <>
2021-03-21 01:48:31 +0000

    Autosave

---
+++ lib/home/portfolio_summary.dart
@@ -0,0 +1,94 @@
+import 'package:fiverr_cachemoney/pay/request_pay.dart';
+import 'package:fiverr_cachemoney/reports/report.dart';
+import 'package:fiverr_cachemoney/widgets/google_nav_bar/google_nav_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class PortfolioSummary extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      // width: 120,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: const FractionalOffset(0.0, 1.0),
+            end: const FractionalOffset(1.0, 0.0),
+            // stops: [0.2, 0.5, 0.7, 0.9],
+            colors: [
+              Color(0xff8AF2FD),
+              Color(0xff5BC7B1),
+              // Color(0xff7DCDE1),
+              Color(0xff148399),
+            ],
+          ),
+          borderRadius: BorderRadius.circular(30)),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Your Portfolio",
+              style: TextStyle(color: Colors.white, fontSize: 15),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "\$450,937",
+              style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Total Profit",
+              style: TextStyle(color: Colors.white, fontSize: 15),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "\$12,484",
+                  style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  color: Color(0xff5BC7B1),
+                  width: 80,
+                  height: 30,
+                  child: Row(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.arrow_drop_up,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "+ 2.5%",
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
