07a8a14b01160cb1d396a4a14ae83d83c1cb0486
FlutLab.io <>
2021-03-21 02:24:35 +0000

    Autosave

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -26,86 +26,9 @@ class _HomeScreenState extends State<HomeScreen> {
       SizedBox(
         height: 10,
       ),
-      Align(
-        alignment: Alignment.centerLeft,
-        child: Text(
-          "Recent Transaction",
-          style: TextStyle(color: Colors.grey, fontSize: 16),
-        ),
-      ),
+      Transactions()
     ]);
-    for (var i = 0; i < 10; i++) {
-      childs.add(new SizedBox(
-        height: 10,
-      ));
-      childs.add(
-        new Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              // height: 100,
-              // width: 300,
-              child: ListTile(
-                dense: true,
-                leading: Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                        Colors.grey.shade800,
-                        Colors.black,
-                      ],
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Icon(
-                      Icons.view_week,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                title: Text(
-                  "Manulife Cash Fund",
-                  style: TextStyle(
-                      fontSize: 18,
-                      // fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                subtitle: Text(
-                  "Mutual Funds",
-                  style: TextStyle(fontSize: 15, color: Colors.grey.shade700),
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.arrow_drop_up,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      "+ 2.5%",
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ],
-        ),
-      );
-    }
+    
     return childs;
   }
 
---
+++ lib/home/transactions.dart
@@ -0,0 +1,100 @@
+import 'package:fiverr_cachemoney/pay/request_pay.dart';
+import 'package:fiverr_cachemoney/reports/report.dart';
+import 'package:fiverr_cachemoney/widgets/google_nav_bar/google_nav_bar.dart';
+import 'package:fiverr_cachemoney/home/portfolio_summary.dart';
+import 'package:fiverr_cachemoney/home/investment_portfolio_list.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Transactions extends StatefulWidget {
+  @override
+  _TransactionsState createState() => _TransactionsState();
+}
+
+class _TransactionsState extends State<Transactions> {
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> childs = [];
+    childs.add(Align(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        "Recent Transaction",
+        style: TextStyle(color: Colors.grey, fontSize: 16),
+      ),
+    ));
+    for (var i = 0; i < 10; i++) {
+      childs.add(new SizedBox(
+        height: 10,
+      ));
+      childs.add(
+        new Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              // height: 100,
+              // width: 300,
+              child: ListTile(
+                dense: true,
+                leading: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Colors.grey.shade800,
+                        Colors.black,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.view_week,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                title: Text(
+                  "Manulife Cash Fund",
+                  style: TextStyle(
+                      fontSize: 18,
+                      // fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                subtitle: Text(
+                  "Mutual Funds",
+                  style: TextStyle(fontSize: 15, color: Colors.grey.shade700),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.arrow_drop_up,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "+ 2.5%",
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+    return Container(children: childs);
+  }
+}
