f56daec030e1480cb08b03733bec0a70477ce0fb
FlutLab.io <>
2021-03-21 01:36:46 +0000

    Autosave

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -16,77 +16,79 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Widget> getList() {
     List<Widget> childs = [];
     for (var i = 0; i < 10; i++) {
-        childs.add(new SizedBox(
-                        height: 10,
-                        ));
-        childs.add(new Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                            Expanded(
-                            // height: 100,
-                            // width: 300,
-                            child: ListTile(
-                                dense: true,
-                                leading: Container(
-                                height: 50,
-                                width: 50,
-                                decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                    begin: Alignment.topLeft,
-                                    end: Alignment.bottomRight,
-                                    colors: [
-                                        Colors.grey.shade800,
-                                        Colors.black,
-                                    ],
-                                    ),
-                                    borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Center(
-                                    child: Icon(
-                                    Icons.view_week,
-                                    color: Colors.white,
-                                    ),
-                                ),
-                                ),
-                                title: Text(
-                                "Manulife Cash Fund",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    // fontWeight: FontWeight.bold,
-                                    color: Colors.white),
-                                ),
-                                subtitle: Text(
-                                "Mutual Funds",
-                                style: TextStyle(fontSize: 15, color: Colors.grey.shade700),
-                                ),
-                            ),
-                            ),
-                            Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                                Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                    Icon(
-                                    Icons.arrow_drop_up,
-                                    color: Colors.white,
-                                    ),
-                                    Text(
-                                    "+ 2.5%",
-                                    style: TextStyle(color: Colors.white, fontSize: 15),
-                                    ),
-                                ],
-                                )
-                            ],
-                            ),
-                        ],
-                        ),)
+      childs.add(new SizedBox(
+        height: 10,
+      ));
+      childs.add(
+        new Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              // height: 100,
+              // width: 300,
+              child: ListTile(
+                dense: true,
+                leading: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Colors.grey.shade800,
+                        Colors.black,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.view_week,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                title: Text(
+                  "Manulife Cash Fund",
+                  style: TextStyle(
+                      fontSize: 18,
+                      // fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                subtitle: Text(
+                  "Mutual Funds",
+                  style: TextStyle(fontSize: 15, color: Colors.grey.shade700),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.arrow_drop_up,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "+ 2.5%",
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      );
     }
     return childs;
-}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -389,144 +391,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     style: TextStyle(color: Colors.grey, fontSize: 16),
                   ),
                 ),
-                getList(),
-                // SizedBox(
-                //   height: 10,
-                // ),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.start,
-                //   crossAxisAlignment: CrossAxisAlignment.center,
-                //   children: [
-                //     Expanded(
-                //       // height: 100,
-                //       // width: 300,
-                //       child: ListTile(
-                //         dense: true,
-                //         leading: Container(
-                //           height: 50,
-                //           width: 50,
-                //           decoration: BoxDecoration(
-                //             gradient: LinearGradient(
-                //               begin: Alignment.topLeft,
-                //               end: Alignment.bottomRight,
-                //               colors: [
-                //                 Colors.grey.shade800,
-                //                 Colors.black,
-                //               ],
-                //             ),
-                //             borderRadius: BorderRadius.circular(10),
-                //           ),
-                //           child: Center(
-                //             child: Icon(
-                //               Icons.view_week,
-                //               color: Colors.white,
-                //             ),
-                //           ),
-                //         ),
-                //         title: Text(
-                //           "Manulife Cash Fund",
-                //           style: TextStyle(
-                //               fontSize: 18,
-                //               // fontWeight: FontWeight.bold,
-                //               color: Colors.white),
-                //         ),
-                //         subtitle: Text(
-                //           "Mutual Funds",
-                //           style: TextStyle(fontSize: 15, color: Colors.grey.shade700),
-                //         ),
-                //       ),
-                //     ),
-                //     Row(
-                //       mainAxisAlignment: MainAxisAlignment.center,
-                //       crossAxisAlignment: CrossAxisAlignment.center,
-                //       children: [
-                //         Row(
-                //           mainAxisAlignment: MainAxisAlignment.center,
-                //           crossAxisAlignment: CrossAxisAlignment.center,
-                //           children: [
-                //             Icon(
-                //               Icons.arrow_drop_up,
-                //               color: Colors.white,
-                //             ),
-                //             Text(
-                //               "+ 2.5%",
-                //               style: TextStyle(color: Colors.white, fontSize: 15),
-                //             ),
-                //           ],
-                //         )
-                //       ],
-                //     ),
-                //   ],
-                // ),
-                // SizedBox(
-                //   height: 10,
-                // ),
-                // Row(
-                //   mainAxisAlignment: MainAxisAlignment.start,
-                //   crossAxisAlignment: CrossAxisAlignment.center,
-                //   children: [
-                //     Expanded(
-                //       // height: 100,
-                //       // width: 300,
-                //       child: ListTile(
-                //         dense: true,
-                //         leading: Container(
-                //           height: 50,
-                //           width: 50,
-                //           decoration: BoxDecoration(
-                //             gradient: LinearGradient(
-                //               begin: Alignment.topLeft,
-                //               end: Alignment.bottomRight,
-                //               colors: [
-                //                 Colors.grey.shade800,
-                //                 Colors.black,
-                //               ],
-                //             ),
-                //             borderRadius: BorderRadius.circular(10),
-                //           ),
-                //           child: Center(
-                //             child: Icon(
-                //               Icons.view_week,
-                //               color: Colors.white,
-                //             ),
-                //           ),
-                //         ),
-                //         title: Text(
-                //           "Manulife Cash Fund",
-                //           style: TextStyle(
-                //               fontSize: 18,
-                //               // fontWeight: FontWeight.bold,
-                //               color: Colors.white),
-                //         ),
-                //         subtitle: Text(
-                //           "Mutual Funds",
-                //           style: TextStyle(fontSize: 15, color: Colors.grey.shade700),
-                //         ),
-                //       ),
-                //     ),
-                //     Row(
-                //       mainAxisAlignment: MainAxisAlignment.center,
-                //       crossAxisAlignment: CrossAxisAlignment.center,
-                //       children: [
-                //         Row(
-                //           mainAxisAlignment: MainAxisAlignment.center,
-                //           crossAxisAlignment: CrossAxisAlignment.center,
-                //           children: [
-                //             Icon(
-                //               Icons.arrow_drop_up,
-                //               color: Colors.white,
-                //             ),
-                //             Text(
-                //               "+ 2.5%",
-                //               style: TextStyle(color: Colors.white, fontSize: 15),
-                //             ),
-                //           ],
-                //         )
-                //       ],
-                //     ),
-                //   ],
-                // ),
-              ],
+              ].addAll(getList()),
             ),
           ),
         ),
